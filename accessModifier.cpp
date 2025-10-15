db2cdc65e407aa85b304003770d3a850278e0634
Tiwari Lokesh <lokeshtiwari212@gmail.com>
2025-10-15 12:34:02 +0530

    Implement code changes to enhance functionality and improve performance

---
+++ accessModifier.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Student{
+public:
+string name;
+int rno;
+
+
+
+Student(string name, int rno, float marks){
+this->name = name;
+this->rno = rno;
+this->marks = marks;
+}
+
+// void display(){
+//     cout<<"Name: "<<name<<endl;
+//     cout<<"Roll No: "<<rno<<endl;
+//     cout<<"Marks: "<<marks<<endl;
+// }
+
+float getMarks(){
+    return marks;
+}
+
+private:
+float marks;
+
+};
+
+int main(){
+Student s1("Alice",101,95.5);
+// s1.display();
+cout<<s1.getMarks();
+}
